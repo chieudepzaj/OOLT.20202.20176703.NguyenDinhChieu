7537247163b3caac548d8c3f339557469642ff70
Your Name <chieumu1999@gmail.com>
2021-05-17 20:39:57 +0700

    Lab10

---
+++ Lab10/src/hust/soict/hedspi/aims/PlayerException.java
@@ -0,0 +1,29 @@
+package hust.soict.hedspi.aims;
+
+public class PlayerException extends Exception {
+
+	public PlayerException() {
+		
+	}
+
+	public PlayerException(String message) {
+		super(message);
+		// TODO
+	}
+
+	public PlayerException(Throwable cause) {
+		super(cause);
+		// TODO 
+	}
+
+	public PlayerException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO 
+	}
+
+	public PlayerException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+		// TODO 
+	}
+
+}
